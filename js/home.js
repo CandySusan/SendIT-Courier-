8a330c9dbbaf9c74cf3b2bb9e3e877942c92dfdc
Candy Susan <candysusan55@gmail.com>
2018-11-06 14:35:53 +0300

    Update the files

--- js/home.js
+++ js/home.js
@@ -1,18 +1,18 @@
 
-function check_input() {
-    var input = document.getElementById("email");
-    if (input.value == "") {
-        document.getElementById("msg").innerHTML = "Email cannot be blank!";
-        input.focus();
-        return false;
-    }
-    re = /^\w+$/;
-    if (!re.test(input.value)) {
-        alert("Error: Email must contain only letters, numbers and underscores!");
-        input.focus();
-        return false;
-    }
+// function check_input() {
+//     var input = document.getElementById("email");
+//     if (input.value == "") {
+//         document.getElementById("msg").innerHTML = "Email cannot be blank!";
+//         input.focus();
+//         return false;
+//     }
+//     re = /^\w+$/;
+//     if (!re.test(input.value)) {
+//         alert("Error: Email must contain only letters, numbers and underscores!");
+//         input.focus();
+//         return false;
+//     }
 
-}
+// }
 
 
